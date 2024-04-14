5478de905838e94b70559abef10770f96b723cc5
NiranjannVarma <167000034+NiranjannVarma@users.noreply.github.com>
2024-04-14 17:45:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+function uploadFile() {
+    var fileInput = document.getElementById('pdfFile');
+    var file = fileInput.files[0];
+
+    var reader = new FileReader();
+    reader.onload = function(event) {
+        var fileData = event.target.result;
+        var params = {
+            fileName: file.name,
+            fileData: fileData
+        };
+        invokeLambdaFunction(params);
+    };
+    reader.readAsDataURL(file);
+}
+
+function invokeLambdaFunction(params) {
+    // Replace 'YOUR_API_GATEWAY_URL' with the actual URL of your deployed API Gateway
+    var apiGatewayUrl = 'https://2fcwkf2zjk.execute-api.us-east-1.amazonaws.com/Developement';
+
+    fetch(apiGatewayUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("File uploaded successfully. Response: ", data);
+        document.getElementById('message').innerHTML = "File uploaded successfully!";
+    })
+    .catch((error) => {
+        console.error("Error uploading file: ", error);
+        document.getElementById('message').innerHTML = "Error uploading file!";
+    });
+}
